820a3ed494bdefce71dcc186adb2d40035fa5a60
Jason Yeung <contact@jasonyeung.me>
2019-09-08 21:01:42 -0700

    simple base implementation

--- src/code.ts
+++ src/code.ts
@@ -1,1 +1,38 @@
 figma.showUI(__html__)
+
+function createBox(padding) {
+  const selection = figma.currentPage.selection
+  const frame = selection[0].parent
+
+  const clone = []
+  
+  for (const node of selection) {
+    const nclone = node.clone()
+    nclone.x = node.absoluteTransform[0][2]
+    nclone.y = node.absoluteTransform[1][2]
+    clone.push(nclone)
+  }
+
+  const group = figma.group(clone, frame)
+  
+  // Create box
+  const { pt, pr, pb, pl } = padding
+  const { x, y, width, height } = group
+  const box = figma.createRectangle()
+  box.fills = [{ type: 'SOLID', color: { r: 1, g: 0, b: 0 } }] // temp
+  box.resize((width + pl + pr), (height + pt + pb))
+  box.x = (x - pl); box.y = (y - pt)
+
+  group.remove()
+  
+  // Insert selection & box as a group
+  frame.insertChild(0, box)
+  figma.currentPage.selection = [box, ...selection]
+}
+
+figma.ui.onmessage = (msg) => {
+  const { type, payload } = msg
+
+  if (type === 'create') createBox(payload)
+  figma.closePlugin()
+}
--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,16 +1,34 @@
-<style type='text/scss'>
-  h1 {
-    color: blue;
-
-    & > * {
-      color: yellow;
-    }
-  }
-</style>
-
 <script>
-  let a = 3
+  const padding = {
+    pt: 0, pb: 0,
+    pl: 0, pr: 0
+  }
+
+  function submit(e) {
+    e.preventDefault()
+    parent.postMessage({ 
+      pluginMessage: {
+        type: 'create',
+        payload: padding
+      }
+    }, '*')
+  }
 </script>
 
-<input bind:value={a} type='number' />
-<h1>REndered! {a}<span>yo this is green</span></h1>
+<form on:submit={submit}>
+  <div>
+    <div>Top</div>
+    <input bind:value={padding.pt} type='number'/>
+
+    <div>Right</div>
+    <input bind:value={padding.pr} type='number'/>
+
+    <div>Bottom</div>
+    <input bind:value={padding.pb} type='number'/>
+
+    <div>Left</div>
+    <input bind:value={padding.pl} type='number'/>
+  </div>
+
+  <button type='submit'>Create</button>
+</form>
